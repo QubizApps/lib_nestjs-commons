352c6e7abac3b98cd0f9b33d04e162870e776f6e
tiberiu89 <tiberiu.popa@qubiz.com>
2022-08-02 18:19:44 +0300

    domain event changes

--- src/ddd/domain/DomainEvent.ts
+++ src/ddd/domain/DomainEvent.ts
@@ -1,8 +1,14 @@
 import { Event } from '../../cqrs/types';
 import { AggregateRoot } from './AggregateRoot';
 
-export type DomainEventPaylod<T extends AggregateRoot = AggregateRoot> = {
-  aggregate: T;
-};
-
-export class DomainEvent<T extends DomainEventPaylod = DomainEventPaylod> extends Event<T> {}
+export class DomainEvent<T = any> extends Event<
+  T & { aggregateId: string; aggregateName: string }
+> {
+  constructor(readonly aggregate: AggregateRoot, payload: T) {
+    super({
+      ...payload,
+      aggregateId: aggregate.id.toString(),
+      aggregateName: aggregate.constructor.name,
+    });
+  }
+}
